a4bd30c41a27d3d60c237ed9d394cd186309b82f
rqphy <raphael.ferreira@hetic.net>
2022-08-29 15:13:49 +0200

    :construction_worker: update light position

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -21,12 +21,12 @@ export default class Environment
 
     setSunlight()
     {
-        this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
+        this.sunLight = new THREE.DirectionalLight('#ffffff', 1.5)
         this.sunLight.castShadow = true
         this.sunLight.shadow.camera.far = 15
         this.sunLight.shadow.mapSize.set(1024, 1024)
         this.sunLight.shadow.normalBias = 0.05
-        this.sunLight.position.set(3.5, 2, - 1.25)
+        this.sunLight.position.set(5, 5, 5)
         this.scene.add(this.sunLight)
 
         // Debug
